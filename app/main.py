7ca2cc751c57818d14c1ddda3be5b5c5c69f1fbd
Luis Gonzalez <166937014+Luisjg02@users.noreply.github.com>
2026-08-07 19:40:06 -0400

    vamo a eto

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
 duro = 2
 
 if duro == 1+1:
-    print("Yarel macagrano")
+    print("Yarel mmaricon-son")    
+
+elif duro == 3:
+
+    print("yarel duro")
+
 else:
-    print("que maricon-son")
+    print("yarel e bacano")
+
+
+
+
+yarel = 1
+hombria = 69
+
+if yarel and hombria >= 70:
+    print("yarel es un hombre")
+
+elif yarel and hombria <= 70:
+    print("yarel es hijo de maricon")
+
+else:
+    print("yarel es un hombre bacano")
+
